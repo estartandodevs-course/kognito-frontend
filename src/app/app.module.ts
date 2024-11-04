050706724335a7847901be8aabce6619391b5ebd
Kassia Albuquerque <kassiademaria@gmail.com>
2024-11-04 15:33:30 -0300

    feat(button): create button component structure and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { InputTextComponent } from './shared/components/input-text/input-text.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ButtonComponent, InputTextComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
