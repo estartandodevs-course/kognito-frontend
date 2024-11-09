36b356d33b8945f4ddf33def4218f82d83d8a3e0
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-09 13:13:42 -0300

    feat(invite card): creating and stylizing invite card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './shared/components/button/button.component';
 import { InputTextComponent } from './shared/components/input-text/input-text.component';
+import { InviteCardComponent } from './shared/components/invite-card/invite-card.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent, InputTextComponent],
+  declarations: [AppComponent, ButtonComponent, InputTextComponent, InviteCardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
