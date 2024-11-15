ad83b190bec24e38bf8dbbcf41cff0dda4c3975c
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-15 17:51:02 -0300

    feat(steps): create steps component with responsive design and TypeScript logic

--- src/app/shared/components/forms/forms.module.ts
+++ src/app/shared/components/forms/forms.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { StepsComponent } from './steps/steps.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
-  exports: [ButtonComponent],
+  declarations: [ButtonComponent, StepsComponent],
+  exports: [ButtonComponent, StepsComponent],
   imports: [CommonModule],
 })
 export class FormsModule {}
