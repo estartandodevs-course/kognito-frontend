8b412a38816938ed0c93298832534228504136fa
Yumi Kato <leilayumi07@gmail.com>
2024-11-16 00:31:51 -0300

    refactor(navbar): simplify html with iconify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//É necessário retirar do comentário o import da Navbar para testá-la
+// import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 const routes: Routes = [{ path: '', loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule) }];
+
+//As rotas abaixo foram comentadas para facilitar o teste da Navbar. Para testar, basta tire do comentário
+// const routes: Routes = [
+//   { path: '', component: NavbarComponent },
+//   { path: 'turma', component: NavbarComponent },
+//   { path: 'tarefas', component: NavbarComponent },
+//   { path: 'ajustes', component: NavbarComponent },
+//   { path: 'metas', component: NavbarComponent },
+// ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
