f40e180f5afbcc0df9f1ca2ddce118ad0b850217
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-17 16:07:20 -0300

    fix(header-desktop): refactoring and improving code

---
+++ src/app/core/components-core/header-desktop/header-desktop.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-desktop',
+  templateUrl: './header-desktop.component.html',
+  styleUrl: './header-desktop.component.scss',
+})
+export class HeaderDesktopComponent {}
