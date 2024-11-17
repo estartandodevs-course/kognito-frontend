787eaf97156e7755648954851308032d6e4ab0c6
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-17 10:54:03 -0300

    feat(header): add desktop header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsCoreModule } from './core/components-core/components-core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, ComponentsCoreModule],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/core/components-core/components-core.module.ts
+++ src/app/core/components-core/components-core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '../../shared/components/forms/forms.module';
+import { DesktopHeaderComponent } from './desktop-header/desktop-header.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, DesktopHeaderComponent],
+  exports: [HeaderComponent, DesktopHeaderComponent],
   imports: [CommonModule, FormsModule],
 })
 export class ComponentsCoreModule {}
