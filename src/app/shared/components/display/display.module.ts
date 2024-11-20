3104a6a8152791f8c68af855e272d08ff0fe183a
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-20 15:18:10 -0300

    feat(component): add classcard-title component

--- src/app/shared/components/display/display.module.ts
+++ src/app/shared/components/display/display.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { FormsModule } from '../forms/forms.module';
 import { SubheaderComponent } from './subheader/subheader.component';
+import { ClasscardTitleComponent } from './classcard-title/classcard-title.component';
 
 @NgModule({
-  declarations: [ModalComponent, SubheaderComponent],
-  exports: [ModalComponent, SubheaderComponent],
+  declarations: [ModalComponent, SubheaderComponent, ClasscardTitleComponent],
+  exports: [ModalComponent, SubheaderComponent, ClasscardTitleComponent],
   imports: [CommonModule, FormsModule],
 })
 export class DisplayModule {}
