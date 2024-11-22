a989ab30722cce86560e81f36a30d617d2c3928e
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-22 10:53:56 -0300

    feat(not-found): add 404 not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FormsModule } from './shared/components/forms/forms.module';
 // import { NavbarModule } from './shared/components/navbar/navbar.module';
 
 @NgModule({
   declarations: [AppComponent, NotFoundComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    // ,NavbarModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss',
+  styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent {}
+export class NotFoundComponent implements OnInit {
+  isTeacherRoute: boolean = false; // Flag para verificar o contexto
+
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  /**
+   * Método do ciclo de vida do Angular chamado após a criação do componente.
+   * Verifica se a rota contém "teacher" para ajustar o contexto.
+   */
+  ngOnInit(): void {
+    this.isTeacherRoute = this.activatedRoute.snapshot.url.some((segment) => segment.path.includes('teacher'));
+  }
+
+  /**
+   * Redireciona o usuário para a página inicial (rota "/").
+   */
+  goToHome(): void {
+    window.location.href = '/';
+  }
+}
