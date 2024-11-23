ce005921e59e367a74ee4b9db4c69b10f257ed16
Cost_Victor <victorcost436@gmail.com>
2024-11-23 13:11:04 -0300

    refactor(header): header component refactoring

--- src/app/shared/components/structural/header/header.component.ts
+++ src/app/shared/components/structural/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { TimeCaptureService } from 'app/core/services/time-capture.service';
+import { IconHeaderProps } from './header.types';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private _time: TimeCaptureService,
+    private _location: Location,
+  ) {}
+
+  @Input() title: string | null = null;
+
+  // Icone com seta é o padrão.
+  @Input() iconsLeft: IconHeaderProps[] = [{ code: 'majesticons:arrow-left', onClick: () => this._location.back() }];
+  @Input() iconsRight?: IconHeaderProps[];
+
+  nameUser = 'Victor Gabriel'; // Obter valor pelo serviço de autenticação.
+  saudation = this._time.getsaudation();
+}
