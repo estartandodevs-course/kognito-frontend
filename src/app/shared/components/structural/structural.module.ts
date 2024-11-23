ee1bdd02424aa28bfbb3a84c05a20cc81cd0f5ab
Cost_Victor <victorcost436@gmail.com>
2024-11-22 21:30:03 -0300

    feat(component section): adding section component to control page responsiveness

--- src/app/shared/components/structural/structural.module.ts
+++ src/app/shared/components/structural/structural.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SectionComponent],
+  exports: [SectionComponent],
   imports: [CommonModule],
 })
-export class LayoutModule {}
+export class StructuralModule {}
