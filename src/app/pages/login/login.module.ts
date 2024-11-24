800a2abf1a3c403e0f3336fd50f76338ed915634
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-24 18:25:01 -0300

    feat(pages): build pages with pre-existing components

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '../../shared/components/forms/forms.module';
+import { StructuralModule } from '../../shared/components/structural/structural.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, FormsModule],
+  imports: [CommonModule, LoginRoutingModule, FormsModule, StructuralModule],
 })
 export class LoginModule {}
