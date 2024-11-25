6542bf9b6a726d99f92ea4bdd456ac8d435749ac
Yumi Kato <leilayumi07@gmail.com>
2024-11-24 22:38:25 -0300

    fix(card-student): badge module was renamed

--- src/app/shared/components/card-student/card-student.module.ts
+++ src/app/shared/components/card-student/card-student.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardStudentComponent } from './card-student.component';
-import { BadgesModule } from '../badges/badges.module';
+import { InterfaceModule } from '../interface/interface.module';
 
 @NgModule({
   declarations: [CardStudentComponent],
   exports: [CardStudentComponent],
-  imports: [CommonModule, BadgesModule],
+  imports: [CommonModule, InterfaceModule],
 })
 export class CardStudentModule {}
