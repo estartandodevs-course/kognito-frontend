5f3c38d05872b04acd6d249b05201d79e6cdc324
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-25 20:52:29 -0300

    feat(create-task): implement steps 1, 2, and 3 in the progress tracker

--- src/app/pages/home/class/create-task/create-task.module.ts
+++ src/app/pages/home/class/create-task/create-task.module.ts
@@ -6,9 +6,14 @@ import { CreateTaskComponent } from './create-task.component';
 import { Step1Component } from './step1/step1.component';
 import { Step2Component } from './step2/step2.component';
 import { Step3Component } from './step3/step3.component';
+import { StructuralModule } from '../../../../shared/components/structural/structural.module';
+import { DisplayModule } from '../../../../shared/components/display/display.module';
+import { InterfaceModule } from '../../../../shared/components/interface/interface.module';
+import { FormsModule } from '../../../../shared/components/forms/forms.module';
+import { NavbarModule } from '../../../../shared/components/navigation/navbar/navbar.module';
 
 @NgModule({
   declarations: [CreateTaskComponent, Step1Component, Step2Component, Step3Component],
-  imports: [CommonModule, CreateTaskRoutingModule],
+  imports: [CommonModule, CreateTaskRoutingModule, StructuralModule, DisplayModule, InterfaceModule, FormsModule, NavbarModule],
 })
 export class CreateTaskModule {}
