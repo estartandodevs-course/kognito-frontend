e09eb1ab902e5f764477b426ed1c00a7b60f1e59
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-26 17:20:36 -0300

    refactor(step-line): refactor code and add JSDoc comments to StepLineComponent

--- src/app/pages/home/class/create-task/create-task.component.ts
+++ src/app/pages/home/class/create-task/create-task.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './create-task.component.html',
   styleUrl: './create-task.component.scss',
 })
-export class CreateTaskComponent {}
+export class CreateTaskComponent {
+  isStepLineEnabled: boolean = false;
+
+  /**
+   * Alterna o estado de habilitação da linha de etapa.
+   *
+   * Esta função altera o valor da variável `isStepLineEnabled` entre `true` e `false`.
+   * Quando o valor de `isStepLineEnabled` é `true`, a linha de etapa é ativada. Quando é `false`, ela é desativada.
+   */
+  toggleStepLine() {
+    this.isStepLineEnabled = !this.isStepLineEnabled;
+  }
+}
--- src/app/pages/home/class/create-task/step-line/step-line.component.ts
+++ src/app/pages/home/class/create-task/step-line/step-line.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-step-line',
@@ -6,16 +6,40 @@ import { Component } from '@angular/core';
   styleUrl: './step-line.component.scss',
 })
 export class StepLineComponent {
+  /**
+   * Define se a linha de etapas está desabilitada ou não.
+   *
+   * A propriedade `disabled` é usada para controlar se a linha de etapas deve ser visualmente desabilitada.
+   * O valor padrão é `false`, o que significa que a linha de etapas está habilitada.
+   */
+  @Input() disabled: boolean = false;
+
+  /** Armazena o passo atual da linha de etapas. */
   currentStep = 1;
 
+  /**
+   * Avança para o próximo passo na linha de etapas.
+   *
+   * A função aumenta o valor de `currentStep` em 1, permitindo que o usuário avance na sequência de etapas.
+   */
   nextStep() {
     this.currentStep = this.currentStep + 1;
   }
 
+  /**
+   * Retrocede para o passo anterior na linha de etapas.
+   *
+   * A função diminui o valor de `currentStep` em 1, permitindo que o usuário retroceda na sequência de etapas.
+   */
   previousStep() {
     this.currentStep = this.currentStep - 1;
   }
 
+  /**
+   * Função que exibe um alerta informando que a funcionalidade ainda não foi implementada.
+   *
+   * Este método é um placeholder que simula a ação de envio, mas ainda não realiza nenhuma operação.
+   */
   submit() {
     alert('AINDA NÃO TA FEITO KKKKKK');
   }
