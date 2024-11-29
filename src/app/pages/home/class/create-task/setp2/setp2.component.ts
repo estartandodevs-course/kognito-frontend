98030db4764a5c3309c4fd0c05124e25bdb3db6b
Kasia Albuquerque <kassiademaria@gmail.com>
2024-11-29 10:40:06 -0300

    feat(steps): add switch behavior at steps component

--- src/app/pages/home/class/create-task/setp2/setp2.component.ts
+++ src/app/pages/home/class/create-task/setp2/setp2.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-setp2',
   templateUrl: './setp2.component.html',
   styleUrl: './setp2.component.scss',
 })
-export class Setp2Component {}
+export class Setp2Component implements OnInit {
+  ngOnInit(): void {
+    console.log('<app-step2> renderizado.');
+  }
+}
--- src/app/pages/home/class/create-task/step1/step1.component.ts
+++ src/app/pages/home/class/create-task/step1/step1.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-step1',
   templateUrl: './step1.component.html',
   styleUrl: './step1.component.scss',
 })
-export class Step1Component {}
+export class Step1Component implements OnInit {
+  ngOnInit(): void {
+    console.log('<app-step1> renderizado.');
+  }
+}
