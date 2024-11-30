7f7db18fe9b823a035e729adb9a0d541ee7573af
Cost_Victor <victorcost436@gmail.com>
2024-11-30 16:30:24 -0300

    chore(structure): adjustments to the develop structure

--- src/app/shared/components/display/display.module.ts
+++ src/app/shared/components/display/display.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ModalComponent } from './modal/modal.component';
 import { AppFormsModule } from '../app-forms/app-forms.module';
+import { ClasscardTitleComponent } from './classcard-title/classcard-title.component';
 
 @NgModule({
-  declarations: [ModalComponent],
-  exports: [ModalComponent],
+  declarations: [ModalComponent, ClasscardTitleComponent],
+  exports: [ModalComponent, ClasscardTitleComponent],
   imports: [CommonModule, AppFormsModule],
 })
 export class DisplayModule {}
