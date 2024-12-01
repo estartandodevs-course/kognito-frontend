9d38693a16153c08ad6d6e0814af83250b13187a
Kasia Albuquerque <kassiademaria@gmail.com>
2024-12-01 10:11:30 -0300

    feat(auth): separate interfaces into a dedicated file

--- src/app/core/services/auth/interceptors/auth.interceptor.ts
+++ src/app/core/services/auth/interceptors/auth.interceptor.ts
@@ -1,28 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { RequestBody, ResponseBody } from './http-interfaces';
 
-interface RequestBody {
-  userId?: string;
-  data?: string;
-}
-
-interface ResponseBody {
-  success: boolean;
-  message: string;
-}
-
+/**
+ * Intercepta requisições HTTP para adicionar cabeçalhos de autenticação, se necessário.
+ *
+ * Este interceptor insere automaticamente um token de autenticação no cabeçalho da requisição, caso ele exista.
+ * Ele utiliza o serviço de autenticação para recuperar o token, garantindo que todas as requisições protegidas
+ * sejam enviadas com credenciais válidas.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptor implements HttpInterceptor {
+  /**
+   * Construtor da classe que inicializa o serviço de autenticação.
+   *
+   * @param authService Serviço responsável por gerenciar a autenticação do usuário.
+   */
   constructor(private authService: AuthService) {}
 
   /**
    * Intercepta as requisições HTTP e adiciona o token de autenticação no cabeçalho, se presente.
    *
-   * Este interceptor verifica se o token de autenticação está disponível. Caso o token exista, ele é
+   * Este método verifica se o token de autenticação está disponível. Caso o token exista, ele é
    * adicionado ao cabeçalho da requisição como um Bearer Token. Se o token não estiver presente, a requisição
    * é feita normalmente sem cabeçalho de autorização.
    *
