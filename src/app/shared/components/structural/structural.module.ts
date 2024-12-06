4e1e183eeba419414de984b5bbcd279d86a096d8
Kasia Albuquerque <kassiademaria@gmail.com>
2024-12-05 22:34:07 -0300

    feat(desktop): adding desktop layout

--- src/app/shared/components/structural/structural.module.ts
+++ src/app/shared/components/structural/structural.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
+import { ImageContainerComponent } from './image-container/image-container.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SectionComponent],
-  exports: [HeaderComponent, SectionComponent],
+  declarations: [HeaderComponent, SectionComponent, ImageContainerComponent],
+  exports: [HeaderComponent, SectionComponent, ImageContainerComponent],
   imports: [CommonModule],
 })
 export class StructuralModule {}
