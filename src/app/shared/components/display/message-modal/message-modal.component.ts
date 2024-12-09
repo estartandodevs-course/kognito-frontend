78032c39843e93cb09da19678d7627f4a7cffe87
Cost_Victor <victorcost436@gmail.com>
2024-12-08 23:10:45 -0300

    feat(error interceptor): error interceptor that opens a modal message

--- src/app/shared/components/display/message-modal/message-modal.component.ts
+++ src/app/shared/components/display/message-modal/message-modal.component.ts
@@ -11,15 +11,27 @@ import { ModalButtonProps } from './message-modal.types';
 export class MessageModalComponent implements OnInit {
   @Input() id!: string;
   @Input() title = 'Título não definido';
-  @Input() message = 'Mensagem não definida';
+  @Input() message: string | string[] = 'Mensagem não definida';
   @Input() buttons?: ModalButtonProps[];
 
-  constructor(private _modalManager: ModalService) {}
+  constructor(private _modalService: ModalService) {}
 
   ngOnInit(): void {
     if (!this.buttons?.length) {
       // Cria um botão 'Fechar' padrão no caso de nenhum botão informado.
-      this.buttons = [{ text: 'Fechar', onClick: () => this._modalManager.removeModal(this.id) }];
+      this.buttons = [{ text: 'Fechar', onClick: () => this._modalService.removeModal(this.id) }];
     }
+  }
+
+  /**
+   * Obtém a mensagem como um array de strings.
+   *
+   * Se a mensagem for um array, ela será retornada como está.
+   * Caso contrário, a mensagem será encapsulada em um array.
+   *
+   * @returns {string[]} A mensagem como um array de strings.
+   */
+  getMessage(): string[] {
+    return Array.isArray(this.message) ? this.message : [this.message];
   }
 }
