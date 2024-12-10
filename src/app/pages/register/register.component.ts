2cd0f94da962ab1a1d3d68d20c012acae088df88
Kasia Albuquerque <kassiademaria@gmail.com>
2024-12-10 20:55:09 -0300

    feat(register): adjust routing

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { authSelectors } from '@store/auth/auth.selectors';
+import { UserProps } from '@store/auth/auth.types';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  user$!: Observable<UserProps | null>;
+
+  constructor(private store: Store) {
+    this.user$ = this.store.select(authSelectors.selectUser);
+  }
+}
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -5,9 +5,11 @@ import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 import { AppFormsModule } from '@components/app-forms/app-forms.module';
 import { StructuralModule } from '@components/structural/structural.module';
-
+import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
+import { HomeViewsModule } from '../../shared/components/dynamicView/home-views/home-views.module';
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterRoutingModule, AppFormsModule, StructuralModule],
+  imports: [CommonModule, RegisterRoutingModule, AppFormsModule, StructuralModule, StudentModule, TeacherModule, HomeViewsModule],
 })
 export class RegisterModule {}
