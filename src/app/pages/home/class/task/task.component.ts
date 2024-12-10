8097ee815c0437d9f44e3746eccd1e1bf0e599a0
Kasia Albuquerque <kassiademaria@gmail.com>
2024-12-10 15:26:53 -0300

    feat(task): add subheader component

--- src/app/pages/home/class/task/task.component.ts
+++ src/app/pages/home/class/task/task.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
-export class TaskComponent {}
+export class TaskComponent {
+  constructor(private router: Router) {}
+
+  /**
+   * Lida com o evento de clique no ícone.
+   * Redireciona o usuário para a página desejada.
+   */
+  onIconClick(): void {
+    this.router.navigate(['/home/class/create_task']);
+  }
+}
