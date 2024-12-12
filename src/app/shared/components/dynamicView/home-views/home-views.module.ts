dd3c66d868193fd56e93797963bb80e4e549720b
Kasia Albuquerque <kassiademaria@gmail.com>
2024-12-12 20:28:16 -0300

    feat(home): add navbar on student home

--- src/app/shared/components/dynamicView/home-views/home-views.module.ts
+++ src/app/shared/components/dynamicView/home-views/home-views.module.ts
@@ -6,10 +6,11 @@ import { TeacherComponent } from './teacher/teacher.component';
 import { StructuralModule } from '@components/structural/structural.module';
 import { AppFormsModule } from '@components/app-forms/app-forms.module';
 import { InterfaceModule } from '../../interface/interface.module';
+import { NavbarModule } from '../../navigation/navbar/navbar.module';
 
 @NgModule({
   declarations: [StudentComponent, TeacherComponent],
   exports: [StudentComponent, TeacherComponent],
-  imports: [CommonModule, StructuralModule, AppFormsModule, InterfaceModule],
+  imports: [CommonModule, StructuralModule, AppFormsModule, InterfaceModule, NavbarModule],
 })
 export class HomeViewsModule {}
