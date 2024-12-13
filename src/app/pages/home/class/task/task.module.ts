fe294f5d7e97f9157101c957fd7a893c68053ae2
Luan Anderson <72465306+LuanAnders0n@users.noreply.github.com>
2024-12-13 20:48:10 -0300

    Revert "style: sidebar"

--- src/app/pages/home/class/task/task.module.ts
+++ src/app/pages/home/class/task/task.module.ts
@@ -4,19 +4,12 @@ import { CommonModule } from '@angular/common';
 import { TaskViewsModule } from '@components/dynamicView/task-views/task-views.module';
 import { TaskRoutingModule } from './task-routing.module';
 import { TaskComponent } from './task.component';
-
-import { AppFormsModule } from '../../../../shared/components/app-forms/app-forms.module';
-import { NavbarModule } from '../../../../shared/components/navigation/navbar/navbar.module';
-import { StructuralModule } from '../../../../shared/components/structural/structural.module';
-import { SidebarModule } from '../../../../shared/components/navigation/sidebar/sidebar.module';
-
 import { AppFormsModule } from '@components/app-forms/app-forms.module';
 import { NavbarModule } from '@components/navigation/navbar/navbar.module';
 import { StructuralModule } from '@components/structural/structural.module';
 
-
 @NgModule({
   declarations: [TaskComponent],
-  imports: [CommonModule, TaskRoutingModule, TaskViewsModule, AppFormsModule, NavbarModule, StructuralModule, SidebarModule],
+  imports: [CommonModule, TaskRoutingModule, TaskViewsModule, AppFormsModule, NavbarModule, StructuralModule],
 })
 export class TaskModule {}
